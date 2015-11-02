0a0df57a86cb832cd1d4b8b444c67133330af278
Stefanie Yoshimura <syoshimura01@gmail.com>
2015-11-01 18:48:42 -0800

    Basic OOP calculator set-up

--- main.js
+++ main.js
@@ -2,21 +2,57 @@
  * Created by Stefanie on 10/29/2015.
  */
 
-function displayValue(type, value, item) {
+/* Pre-made calculator.js hook-in code
 
-    $('#number-readout').text(value);
+ function displayValue(type, value, item) {
 
-}
+ $('#number-readout').text(value);
 
-var my_calculator = new calculator(displayValue);
+ }
+
+ var my_calculator = new calculator(displayValue);
+
+ $(document).ready(function () {
+
+ $('.button-wrapper').on('click', 'button', function () {
+
+ var val = $(this).text();
+ my_calculator.addItem(val);
+
+ })
+
+ });*/
+
+//OOP Calculator First-attempt
+
+var calculationInput = [];
+
+var calculations = function (value) {
+
+    this.value = value;
+
+    this.inputValue = function (value) {
+      calculationInput.push(value);
+    };
+
+    this.calcDisplay = function (value) {
+        $('#number-readout').text(value);
+    };
+
+};
+
+var calculator = new calculations();
 
 $(document).ready(function () {
 
     $('.button-wrapper').on('click', 'button', function () {
 
+        console.log('clicked');
         var val = $(this).text();
-        my_calculator.addItem(val);
+        calculator.calcDisplay(val);
+        calculator.inputValue(val);
+        console.log(calculationInput);
 
-    })
+    });
 
 });
