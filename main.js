0db891c6730da0a286170b3b3057c5bb66e1c973
Stefanie Yoshimura <syoshimura01@gmail.com>
2015-11-02 18:11:46 -0800

    Decimal point functionality

--- main.js
+++ main.js
@@ -30,7 +30,20 @@ var calculations = function (value) {
 
     this.value = value;
 
+    this.canAddDecimal = true;
+
+    //var canAddDecimal = (function () {
+    //    var decimalToggle = false;
+    //    return function () {return decimalToggle = !decimalToggle;}
+    //})();
+
     this.inputValue = function (value) {
+
+        var newInput = {
+            value: '',
+            type: '',
+            hasDecimal: false
+        };
 
         switch (value) {
             case 'C':
@@ -40,7 +53,7 @@ var calculations = function (value) {
 
             case 'CE':
                 calculationInput.pop();
-                this.calcDisplay(calculationInput[calculationInput.length - 1]);
+                this.calcDisplay(calculationInput[calculationInput.length - 1].value);
                 break;
 
             case '=':
@@ -59,43 +72,80 @@ var calculations = function (value) {
             case '-':
             case '*':
             case '/':
-                calculationInput.push(value);
+                if (calculationInput.length >= 1 && calculationInput[calculationInput.length - 1].type == 'operator') {
+                    calculationInput[calculationInput.length - 1].value = value;
+                }
+
+                else {
+                    newInput.value = value;
+                    newInput.type = 'operator';
+                    calculationInput.push(newInput);
+                }
+
+                this.calcDisplay(calculationInput[calculationInput.length -1].value);
+                console.log(calculationInput);
+                break;
+
+            case '.':
+                if (calculationInput.length >= 1 && !calculationInput[calculationInput.length - 1].hasDecimal) {
+                    if (calculationInput.length >= 1 && calculationInput[calculationInput.length - 1].type == 'number') {
+                        calculationInput[calculationInput.length - 1].value += value;
+                        calculationInput[calculationInput.length - 1].hasDecimal = true;
+                    }
+                }
+
+                else {
+                    return
+                }
+                this.calcDisplay(calculationInput[calculationInput.length -1].value);
+                console.log(calculationInput);
                 break;
 
             default:
-                calculationInput.push(value);
-                this.calcDisplay(value);
+                if (calculationInput.length >= 1 && calculationInput[calculationInput.length - 1].type == 'number') {
+                    calculationInput[calculationInput.length - 1].value += value;
+                }
+
+                else {
+                    newInput.value = value;
+                    newInput.type = 'number';
+                    calculationInput.push(newInput);
+                }
+
+                this.calcDisplay(calculationInput[calculationInput.length -1].value);
                 console.log(calculationInput);
                 break;
         }
 
     };
 
+    //Shows current value in number readout
     this.calcDisplay = function (value) {
 
         $('#number-readout').text(value);
 
     };
 
+    //Performs calculations
     this.processInputs = function () {
 
         var output = null;
 
-        switch (calculationInput[1]) {
+        switch (calculationInput[1].value) {
             case '+':
-                output = parseFloat(calculationInput[0]) + parseFloat(calculationInput[2]);
+                output = parseFloat(calculationInput[0].value) + parseFloat(calculationInput[2].value);
                 break;
 
             case '-':
-                output = parseFloat(calculationInput[0]) - parseFloat(calculationInput[2]);
+                output = parseFloat(calculationInput[0].value) - parseFloat(calculationInput[2].value);
                 break;
 
             case '*':
-                output = parseFloat(calculationInput[0]) + parseFloat(calculationInput[2]);
+                output = parseFloat(calculationInput[0].value) * parseFloat(calculationInput[2].value);
                 break;
 
             default:
-                output = parseFloat(calculationInput[0]) / parseFloat(calculationInput[2]);
+                output = parseFloat(calculationInput[0].value) / parseFloat(calculationInput[2].value);
         }
         return output
 
